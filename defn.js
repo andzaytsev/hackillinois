783f2aeb0cfbb00ed1ab886bc79dd58e0419c500
temandr <brentwoodpav@gmail.com>
2014-04-12 17:32:16 -0500

    I'm not sure if it works or not

--- defn.js
+++ defn.js
@@ -7,7 +7,7 @@
 * This object is used for each node
 * It has three properties: 
 * ->beginning		=	the location of the start of the phrase (inclusive)
-* ->end 		= 	the end of the phrase (not inclusive)
+* ->end		= 	the end of the phrase (not inclusive)
 * ->comment		=	the text that the object holds (user input)
 * ->key		=	This is the text that the person highlighted
 * ->outKey		=	the type in the html file... also used as a key
@@ -29,14 +29,14 @@ function node(beg, end, comm, key, outKey) {
 function Site(url){
 	this.url 			= url; 
 	this.length		=  0;
-	this.node 		= new Array();
+	this.nodes 		= new Array();
 }
 
 /**
  * Insert a new node
  */
 Site.prototype.insert  = function(beg, end, comm, key, outKey) {
-	this.node.push(new node(beg, end, comm, key, outKey));
+	this.nodes.push(new node(beg, end, comm, key, outKey));
 	this.length += 1;
 };
 
@@ -45,7 +45,11 @@ Site.prototype.insert  = function(beg, end, comm, key, outKey) {
   */
 Site.prototype.storeToChrome = function() {
 	//Create the JSON representation of the array
-	localStorage["defn" + this.url] = JSON.stringify(this.node);
+	//localStorage["defn" + this.url] = JSON.stringify(this.node);
+	chrome.storage.local.set( {'defn' : this.nodes} , function() 
+		{
+			message("Saved Comment");
+		});
 };
 
  /**
@@ -53,14 +57,16 @@ Site.prototype.storeToChrome = function() {
   */
 Site.prototype.getFromChrome= function() {
 	//Clear the array
-	this.node = [];
+	this.nodes = [];
 	length = 0;
 
 	//Retrieve the array from storage
-	this.node = JSON.parse(localStorage["defn"+this.url]);
-	this.length = this.node.length;
+//	this.node = JSON.parse(localStorage["defn"+this.url]);
+	chrome.storage.local.get( 'defn', function (result) {
+		this.nodes = result.defn;
+	});
+	this.length = this.nodes.length;
 };
-
  /**
   * testing
   */
@@ -73,7 +79,10 @@ function aplay() {
 	var test5 = "outKey"; 
 	var site1 = new Site("google.com");
 	site1.insert(test1, test2, test3, test4, test5);
-	alert(site1.node[0].beginning);
+	alert(site1.nodes[0].beginning);
 	site1.storeToChrome();
 	alert("ehehehehehe");
+	//site1.getFromChrome();
+
+	alert(site1.nodes[0].beginning);
 }
