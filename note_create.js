fe6ddaa18a787f67acecfb73cf53943beb5d8c78
Andrey Zaytsev <zaytsev2@illinois.edu>
2014-04-12 14:54:34 -0500

    Added initial version of note_create

---
+++ note_create.js
@@ -0,0 +1,15 @@
+/**
+ * This file has functions to add a note by injecting
+ * JavaScript into a page
+ * author: Andrey Zaytsev
+ */
+
+/**
+ * Class for creating a new note 
+ */
+var noteCreator = {
+	displayDialog: function() {
+		console.log('clicked AddNote!!!')
+		alert('ok');
+	}
+};
